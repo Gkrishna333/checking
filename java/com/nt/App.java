e3d4cbe4c150b6b2c22ff6a83e27aaa0a08d2bdc
saikrishna <gadadas.saikrishna@gmail.com>
2022-04-28 18:06:31 +0530

    intial project

---
+++ java/com/nt/App.java
@@ -0,0 +1,23 @@
+package com.nt;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App {
+
+	
+		
+	
+
+	public static void main(String[] args) {
+m1();
+		System.out.println("sas");
+	}
+}
